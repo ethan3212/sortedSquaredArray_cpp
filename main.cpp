fe5f2fbc9078cae24f941bb09a7a25e12bfa71ba
ethan3212 <ethan.g.manis@gmail.com>
2022-09-08 14:12:51 -0600

    Improved readability.

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 // O(nLog(n)) time | O(n) space
 vector<int> sortedSquaredArraySort(vector<int> array) {
-    vector<int> squaredNumbers(array.size());       // O(n) space
-    for(int i = 0; i < array.size(); i++) {     // O(n) time
+    vector<int> squaredNumbers(array.size());// O(n) space
+    for(int i = 0; i < array.size(); i++) {// O(n) time
         squaredNumbers[i] = array[i] * array[i];
     }
-    sort(squaredNumbers.begin(), squaredNumbers.end());     // O(nLog(n)) time
+    sort(squaredNumbers.begin(), squaredNumbers.end());// O(nLog(n)) time
     return squaredNumbers;
 }
 
 // O(n) time | O(n) space
 vector<int> sortedSquaredArrayReverseFill(vector<int> array) {
-    vector<int> squaredNumbers(array.size());       // O(n) space
+    vector<int> squaredNumbers(array.size());// O(n) space
     int left = 0;
     int right = array.size() - 1;
-    for(int i = array.size() - 1; i >= 0; i--) {        // O(n) time
+    for(int i = array.size() - 1; i >= 0; i--) {// O(n) time
         if(abs(array[left]) > abs(array[right])) {
             squaredNumbers[i] = array[left] * array[left];
             left++;
